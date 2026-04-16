a3011becf77c30dbf751763bd155f5ca6f504947
robotaccountdevconnect26 <bhavna.iyengar@gmail.com>
2026-04-16 11:22:06 -0400

    Update agent.py

--- atlas_three_agent/agent.py
+++ atlas_three_agent/agent.py
@@ -3,6 +3,6 @@ from google.adk.agents.llm_agent import Agent
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description='A helpful assistant to give interesting tasks about cities.',
+    instruction='Always give 3 random interesting facts about cities for kids, reject any other query',
 )
